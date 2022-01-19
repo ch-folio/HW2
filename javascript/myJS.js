a21cbacb14e4be1d62f7af5ba8c44322a92fd43b
ch-folio <chelhuds@gmail.com>
2022-01-18 18:05:14 -0800

    Added html, css, and java for completion of the week 2 homework

---
+++ javascript/myJS.js
@@ -0,0 +1,52 @@
+// runs immediately when the page is loaded
+
+ 
+let word = [];
+
+
+console.log(word);
+
+// runs after the DOM is completely loaded
+document.addEventListener("DOMContentLoaded", function (event) {
+   
+    document.getElementById("start").style.visibility = "hidden";
+    document.getElementById("swapWords").style.display = "none";
+    document.getElementById("start").addEventListener("click", function() {
+        swap()
+    });
+    
+
+    for( let i=0; i<3; i++) {
+        word[i]   = prompt("Please enter a word:");
+    }
+    
+    let ulVar = document.getElementById('myUL');
+    for (let i = 0; i < word.length; i++) {
+        let liVar = document.createElement('li');
+        liVar.innerHTML = word[i];
+        ulVar.appendChild(liVar);
+    };
+
+    document.getElementById("start").style.visibility = "visible";
+
+});
+
+
+
+// runs only when called)
+function swap() {
+
+    let swapped = word.map(function(oneWord) {
+        return oneWord.charAt(oneWord.length - 1) + oneWord.substring(1, oneWord.length - 1) + oneWord.charAt(0); });
+
+
+    let ulVar2 = document.getElementById('myUL2');
+    for (let i = 0; i < swapped.length; i++) {
+        let liVar = document.createElement('li');
+        liVar.innerHTML = swapped[i];
+        ulVar2.appendChild(liVar);
+    };
+
+    document.getElementById("swapWords").style.display = "block";
+    document.getElementById("GetWords").style.display = "none";
+}
